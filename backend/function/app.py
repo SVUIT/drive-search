2c4c0c5190ee789e476bd44847ae8bbacd908994
Doan Nguyen Lam <22520736@gm.uit.edu.vn>
2025-07-04 21:32:45 +0700

    fix: add context

--- backend/function/app.py
+++ backend/function/app.py
@@ -1,32 +1,32 @@
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
 from dotenv import load_dotenv
 from function.fetch_changes import fetch_changes
 from function.page_token_utils import read_start_page_token, save_start_page_token
-
+import json
 load_dotenv()
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/check-changes", methods=["GET"])
-def check_changes():
+# @app.route("/check-changes", methods=["GET"])
+def main(context):
     try:
         saved_token = read_start_page_token()
         saved_token, file_details = fetch_changes(saved_start_page_token=saved_token)
 
         save_start_page_token(saved_token)
         if not file_details:
-            return jsonify({"message": "No changes detected", 
+            return context.res.json({"message": "No changes detected", 
                             "page_token": saved_token
             })
 
-        return jsonify({
+        return context.res.json({
             "message": "Changes detected",
             "page_token": saved_token,
             "changes": file_details
         })
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return context.res.json({"error": str(e)})
 
 
 # if __name__ == "__main__":
