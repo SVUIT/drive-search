45a6849b841c0a4da4c4a0d4f04c9a313bfdc370
Doan Nguyen Lam <22520736@gm.uit.edu.vn>
2025-07-04 21:20:04 +0700

    fix: remove main function

--- backend/function/app.py
+++ backend/function/app.py
@@ -29,5 +29,5 @@ def check_changes():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
