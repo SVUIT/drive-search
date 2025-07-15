88ceae998ce71093c5f419e5c4e0c75ee3d723bb
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-07-15 14:51:36 +0000

    fix 400 error

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -76,7 +76,9 @@ async function performSearch() {
 
         if (query) params.append('query', query);
         if (selectedTags.length > 0) params.append('tags', selectedTags.join(','));
-        if (!query && hasSubjectCode) params.append('documents', selected.code);
+        if (!query && selected && selected.code) {
+            params.append('documents', selected.code);
+        }
 
         if (params.toString()) url += '?' + params.toString();
         const response = await fetch(url);
